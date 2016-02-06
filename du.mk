b4becc3d7a1aee6c6fd237b29e12cb06e62551f9
Andrea Turchet (aka rap_turkez) <andre.turky@gmail.com>
2016-02-06 19:12:56 +0100

    Update and rename validus.mk to du.mk

--- du.mk
+++ du.mk
@@ -2,23 +2,23 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
-# Inherit some common Validus stuff.
+# Inherit some common DU stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00A/device.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := validus_Z00A
+PRODUCT_NAME := du_Z00A
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := AOSP on Z00A
 PRODUCT_MANUFACTURER := asus
